715c18e9943baa8ca1387fd6a96a5655e444d5f3
Guillaume Ginet <guillaume.ginet@gmail.com>
2026-09-22 16:55:45 +0200

    fix: missing env var pushed to execution

--- test/examples_basic_test.go
+++ test/examples_basic_test.go
@@ -10,10 +10,9 @@ func TestExamplesBasic(t *testing.T) {
 
 	terraformOptions := &terraform.Options{
 		TerraformDir: "../examples/basic",
-		// Vars: map[string]interface{}{
-		// 	"myvar":     "test",
-		// 	"mylistvar": []string{"list_item_1"},
-		// },
+		Vars: map[string]interface{}{
+			"project_id": os.Getenv("STACKIT_PROJECT_ID"),
+		},
 	}
 
 	defer terraform.Destroy(t, terraformOptions)
